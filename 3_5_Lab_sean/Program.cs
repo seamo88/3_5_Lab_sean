02e01a580354989bfac37c54b52435991b3cde22
seamo88 <bstockgame.88@gmail.com>
2020-02-23 12:57:32 -0600

    Add project files.

---
+++ 3_5_Lab_sean/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _3_5_Lab_sean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string blackListWord = "bomb";
+            bool isSpam = false;
+            string message = Console.ReadLine();
+            if (message.Contains(blackListWord))
+            {
+                isSpam = true;
+                Console.WriteLine("The message is spam");
+            }
+            else
+            {
+                isSpam = false;
+                Console.WriteLine("The message is not spam.");
+            }
+        }
+    }
+}
